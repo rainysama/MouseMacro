95f63b601130a6baec853b8951dc351414318607
Rainysama <rainyssama@gmail.com>
2021-12-16 20:01:53 +0300

    Change loop method

--- MouseMacro/MouseMacro.cs
+++ MouseMacro/MouseMacro.cs
@@ -28,6 +28,9 @@ namespace Helper
         private Func<int> m_getRepeatedMouseCount;
         private Func<MouseButtons> m_getCurrentMouseButton;
 
+        private int m_remainCountToClick = 0;
+        private uint m_mouseX, m_mouseY = 0;
+
         public MouseMacro(Func<string> getSelectedProcessName, Func<int> getrepeatedMouseCount, Func<MouseButtons> getCurrentMouseButton, Func<bool> isRunning)
         {
             m_getSelectedProcessName = getSelectedProcessName;
@@ -49,7 +52,14 @@ namespace Helper
             try
             {
                 while (true)
-                    continue;
+                {
+                    if (m_remainCountToClick > 1)
+                    {
+                        mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, m_mouseX, m_mouseY, 0, 0);
+                        m_remainCountToClick--;                        
+                        Thread.Sleep(100);
+                    }
+                }
             } catch (ThreadAbortException)
             {
                 Dispose();
@@ -64,12 +74,10 @@ namespace Helper
             var activatedProcessFileName = GetActiveProcessFileName();
 
             if (e.Button == m_getCurrentMouseButton() && activatedProcessFileName == m_getSelectedProcessName())
-            {                                
-                for (int i = 0; i < m_getRepeatedMouseCount(); ++i)
-                {                    
-                    mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)e.X, (uint)e.Y, 0, 0);
-                }
-
+            {
+                m_mouseX = (uint)e.X;
+                m_mouseY = (uint)e.Y;
+                m_remainCountToClick += m_getRepeatedMouseCount();
                 e.Handled = true;
             }
         }
