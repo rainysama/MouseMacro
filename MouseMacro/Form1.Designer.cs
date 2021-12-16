c77ce5ebe38a5754358953ad6cb5b637adc87564
Mehmetz <Mehmetz@MEHMET>
2021-12-16 18:19:20 +0300

    Add project files.

---
+++ MouseMacro/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace MouseMacro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.processListBox = new System.Windows.Forms.ListBox();
+            this.repeatProcessCountLabel = new System.Windows.Forms.Label();
+            this.repeatMouseCount = new System.Windows.Forms.NumericUpDown();
+            this.mouseButtonName = new System.Windows.Forms.Label();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.mouseNameCombobBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.repeatMouseCount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // processListBox
+            // 
+            this.processListBox.FormattingEnabled = true;
+            this.processListBox.Location = new System.Drawing.Point(12, 12);
+            this.processListBox.Name = "processListBox";
+            this.processListBox.Size = new System.Drawing.Size(229, 329);
+            this.processListBox.TabIndex = 0;
+            // 
+            // repeatProcessCountLabel
+            // 
+            this.repeatProcessCountLabel.AutoSize = true;
+            this.repeatProcessCountLabel.Location = new System.Drawing.Point(12, 360);
+            this.repeatProcessCountLabel.Name = "repeatProcessCountLabel";
+            this.repeatProcessCountLabel.Size = new System.Drawing.Size(116, 13);
+            this.repeatProcessCountLabel.TabIndex = 1;
+            this.repeatProcessCountLabel.Text = "İşlem tekrarlama miktarı";
+            // 
+            // repeatMouseCount
+            // 
+            this.repeatMouseCount.Location = new System.Drawing.Point(149, 360);
+            this.repeatMouseCount.Name = "repeatMouseCount";
+            this.repeatMouseCount.Size = new System.Drawing.Size(92, 20);
+            this.repeatMouseCount.TabIndex = 2;
+            // 
+            // mouseButtonName
+            // 
+            this.mouseButtonName.AutoSize = true;
+            this.mouseButtonName.Location = new System.Drawing.Point(12, 393);
+            this.mouseButtonName.Name = "mouseButtonName";
+            this.mouseButtonName.Size = new System.Drawing.Size(108, 13);
+            this.mouseButtonName.TabIndex = 4;
+            this.mouseButtonName.Text = "Kullanılacak fare tuşu";
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(15, 426);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(105, 23);
+            this.startBtn.TabIndex = 5;
+            this.startBtn.Text = "Başlat";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(138, 426);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(103, 23);
+            this.stopBtn.TabIndex = 6;
+            this.stopBtn.Text = "Durdur";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // mouseNameCombobBox
+            // 
+            this.mouseNameCombobBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.mouseNameCombobBox.FormattingEnabled = true;
+            this.mouseNameCombobBox.Location = new System.Drawing.Point(149, 399);
+            this.mouseNameCombobBox.Name = "mouseNameCombobBox";
+            this.mouseNameCombobBox.Size = new System.Drawing.Size(91, 21);
+            this.mouseNameCombobBox.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 461);
+            this.Controls.Add(this.mouseNameCombobBox);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.mouseButtonName);
+            this.Controls.Add(this.repeatMouseCount);
+            this.Controls.Add(this.repeatProcessCountLabel);
+            this.Controls.Add(this.processListBox);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(268, 500);
+            this.Name = "Form1";
+            this.Text = "Mouse Macro";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.repeatMouseCount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox processListBox;
+        private System.Windows.Forms.Label repeatProcessCountLabel;
+        private System.Windows.Forms.NumericUpDown repeatMouseCount;
+        private System.Windows.Forms.Label mouseButtonName;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.ComboBox mouseNameCombobBox;
+    }
+}
+
---
+++ MouseMacro/Form1.cs
@@ -0,0 +1,140 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace MouseMacro
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, out uint ProcessId);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+
+        private IKeyboardMouseEvents m_GlobalHook;
+        private const uint MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const uint MOUSEEVENTF_LEFTUP = 0x04;
+        private bool isRunning = false;
+        private MouseButtons currentMouseButton = MouseButtons.XButton2;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            m_GlobalHook = Hook.GlobalEvents();
+            m_GlobalHook.MouseDownExt += OnMouseDown;
+
+            stopBtn.Enabled = false;
+            
+            RefreshProcessList();
+            RefreshMouseButtonNamesList();
+        }
+
+        private void RefreshMouseButtonNamesList()
+        {            
+            mouseNameCombobBox.Items.Clear();
+            mouseNameCombobBox.Items.Add(MouseButtons.None);
+            mouseNameCombobBox.Items.Add(MouseButtons.XButton1);
+            mouseNameCombobBox.Items.Add(MouseButtons.XButton2);
+
+            mouseNameCombobBox.SelectedIndex = mouseNameCombobBox.FindStringExact("XButton2");
+        }
+
+        private void OnMouseDown(object sender, MouseEventExtArgs e)
+        {
+            if (!isRunning)            
+                return;
+            
+            var activatedProcessFileName = GetActiveProcessFileName();
+            if (e.Button == currentMouseButton && activatedProcessFileName == processListBox.SelectedItem.ToString())
+            {
+                Console.WriteLine("Heyo works");
+                return;
+
+                for (int i = 0; i < repeatMouseCount.Value; ++i)
+                    mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)e.X, (uint)e.Y, 0, 0);
+
+                e.Handled = true;
+            }            
+        }
+
+        private void RefreshProcessList()
+        {
+            processListBox.Items.Clear();            
+            Process[] processCollection = Process.GetProcesses();
+            foreach (Process p in processCollection)
+                processListBox.Items.Add(p.ProcessName);
+        }
+
+        private string GetActiveProcessFileName()
+        {
+            IntPtr hwnd = GetForegroundWindow();
+            uint pid;
+            GetWindowThreadProcessId(hwnd, out pid);
+            Process p = Process.GetProcessById((int)pid);
+            return p.ProcessName;
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            if (repeatMouseCount.Value < 1)
+            {
+                MessageBox.Show("İşlem tekrar miktarı 1'den küçük olduğu için devam edilemez.", "Mouse Macro", MessageBoxButtons.OK);
+                return;
+            }
+
+            if (processListBox.SelectedItem == null)
+            {
+                MessageBox.Show("İşlemin çalışacağı pencere belirtilmeden işleme devam edilemez.", "Mouse Macro", MessageBoxButtons.OK);
+                return;
+            }
+
+            processListBox.Enabled = false;
+            repeatMouseCount.Enabled = false;
+            mouseNameCombobBox.Enabled = false;
+
+            currentMouseButton = (MouseButtons)Enum.Parse(typeof(MouseButtons), mouseNameCombobBox.SelectedItem?.ToString());
+            isRunning = true;
+            startBtn.Enabled = false;
+            stopBtn.Enabled = true;
+        }
+
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            processListBox.Enabled = true;
+            repeatMouseCount.Enabled = true;
+            mouseNameCombobBox.Enabled = true;
+
+            isRunning = false;
+            startBtn.Enabled = true;
+            stopBtn.Enabled = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (isRunning)
+            {
+                var result = MessageBox.Show("Şuanda mouse makro çalışmakta. Kapatmak istiyor musunuz?", "Mouse Macro", MessageBoxButtons.YesNo);
+                if (result == DialogResult.No)
+                {
+                    e.Cancel = true;
+                    return;
+                }                
+            }
+
+            m_GlobalHook.MouseDownExt -= OnMouseDown;
+            m_GlobalHook.Dispose();
+        }
+    }
+}
